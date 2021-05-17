c327a4b6802dfebe032ab81674895bb85f307d08
Marcelo Lacerda <marceloslacerda@gmail.com>
2021-05-17 00:54:05 +0000

    Remove docker-compose exec tty

--- docker-stuff/bootstrap-run.sh
+++ docker-stuff/bootstrap-run.sh
@@ -9,12 +9,12 @@ touch forensic_log
 curl -s https://api.github.com/repos/glpi-project/glpi/releases/latest | grep browser_download_url | cut -d : -f 2,3 | tr -d \" | xargs curl -sL  | tar -xz
 mv glpi webapp
 docker-compose up -d
-until docker-compose exec db mysql -uroot -pexample glpi --execute 'SELECT 1' ; do
+until docker-compose -T exec db mysql -uroot -pexample glpi --execute 'SELECT 1' ; do
     sleep 1
 done
 docker-compose exec -T db mysql -u root -pexample glpi < database_dump.sql
-docker-compose exec -u www-data webapp php bin/console glpi:system:check_requirements
-docker-compose exec -u www-data webapp php bin/console glpi:maintenance:enable || true
+docker-compose exec -T -u www-data webapp php bin/console glpi:system:check_requirements
+docker-compose exec -T -u www-data webapp php bin/console glpi:maintenance:enable || true
 docker-compose exec -T -u www-data webapp php bin/console db:configure -r -H db -d glpi -u root -p example
-docker-compose exec -u www-data webapp php bin/console db:update --allow-unstable
-docker-compose exec -u www-data webapp php bin/console glpi:maintenance:disable || true
+docker-compose exec -T -u www-data webapp php bin/console db:update --allow-unstable
+docker-compose exec -T -u www-data webapp php bin/console glpi:maintenance:disable || true
