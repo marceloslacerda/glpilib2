cacba45ef1006f61f322e0f1594cd3a4a603d294
Marcelo Lacerda <marceloslacerda@gmail.com>
2021-05-17 00:46:00 +0000

    Further fixes to github ci
    Fixed: Wait for database to become availabe
    Fixed: Reintroduce download latest stable glpi version step
    Refactor: Use docker-compose exec where possible

--- docker-stuff/bootstrap-run.sh
+++ docker-stuff/bootstrap-run.sh
@@ -1,18 +1,20 @@
 #!/bin/sh -xe
 
+docker-compose down
 rm -rf webapp
 rm -rf glpi
 mkdir -p webapp
 mkdir -p database
 touch forensic_log
-#curl -s https://api.github.com/repos/glpi-project/glpi/releases/latest | grep browser_download_url | cut -d : -f 2,3 | tr -d \" | xargs curl -sL  | tar -xz
-#mv glpi webapp
-docker-compose up &
-sleep 5
-docker exec -i docker-stuff_db_1 mysql -u root -pexample glpi < database_dump.sql
-# run install routine on glpi
-docker exec -u www-data docker-stuff_webapp_1 php bin/console glpi:system:check_requirements
-docker exec -u www-data docker-stuff_webapp_1 php bin/console glpi:maintenance:enable || true
-docker exec -u www-data docker-stuff_webapp_1 php bin/console db:configure -r -H db -d glpi -u root -p example
-docker exec -u www-data docker-stuff_webapp_1 php bin/console db:update
-docker exec -u www-data docker-stuff_webapp_1 php bin/console glpi:maintenance:disable || true
+curl -s https://api.github.com/repos/glpi-project/glpi/releases/latest | grep browser_download_url | cut -d : -f 2,3 | tr -d \" | xargs curl -sL  | tar -xz
+mv glpi webapp
+docker-compose up -d
+until docker-compose exec db mysql -uroot -pexample glpi --execute 'SELECT 1' ; do
+    sleep 1
+done
+docker-compose exec -T db mysql -u root -pexample glpi < database_dump.sql
+docker-compose exec -u www-data webapp php bin/console glpi:system:check_requirements
+docker-compose exec -u www-data webapp php bin/console glpi:maintenance:enable || true
+docker-compose exec -T -u www-data webapp php bin/console db:configure -r -H db -d glpi -u root -p example
+docker-compose exec -u www-data webapp php bin/console db:update --allow-unstable
+docker-compose exec -u www-data webapp php bin/console glpi:maintenance:disable || true
